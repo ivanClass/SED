3bb5def5a5aaf7549ec6697d1baf35c6616eb03a
ivanClass <ivan.aguilera103@gmail.com>
2018-05-22 19:00:04 +0200

    Añadido barra menú

--- RFIDoors/Parte interna - administrador/rfidoors-app/src/index.js
+++ RFIDoors/Parte interna - administrador/rfidoors-app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
