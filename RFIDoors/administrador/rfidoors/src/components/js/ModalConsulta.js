16a2e7f4a97536c0bb5287c6def59a8ae17de3a5
ivanClass <ivan.aguilera103@gmail.com>
2018-05-24 10:53:01 +0200

    no message

--- RFIDoors/administrador/rfidoors/src/components/js/ModalConsulta.js
+++ RFIDoors/administrador/rfidoors/src/components/js/ModalConsulta.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import FaSearch from 'react-icons/lib/fa/search';
-import log from './start.js';
+//import log from './start.js';
 
 class ModalConsulta extends React.Component {
   constructor(props) {
@@ -11,18 +11,23 @@ class ModalConsulta extends React.Component {
     };
 
     this.toggle = this.toggle.bind(this);
+    this.ejecutaPython = this.ejecutaPython.bind(this);
   }
 
   toggle() {
     this.setState({
       modal: !this.state.modal
     });
-    log();
+    //log();
+  }
+
+  ejecutaPython(){
+    //log();
   }
 
   render() {
     return (
-    	<Button onClick={this.toggle}><FaSearch size={40}/>Consultar Tarjeta
+    	<Button onClick={this.ejecutaPython}><FaSearch size={40}/>Consultar Tarjeta
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Consultando Acceso de Tarjeta</ModalHeader>
           <ModalBody>
